306b2a4684abad685f9680521ba5fe1a807716bd
unknown <jabri.oldd@gmail.com>
2022-01-22 20:34:33 +0100

    making an intro animation with gsap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
+import gsap from "gsap";
 import Banner from "./Components/Banner";
 import Cases from "./Components/Cases";
 import Header from "./Components/Header";
+import Intro from "./Components/Intro";
 import "./styles/App.scss";
 
 function App() {
@@ -10,9 +12,45 @@ function App() {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
     //making it responsive and taking 50vh from the page on mobile devices
     //handling device and browser-specific
+    //making flashes dissapear
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+    //timeLine Animation
+    const tl = gsap.timeline();
+    tl.from(".line span", 1.6, {
+      y: 100,
+      ease: "power4.out",
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+      .to(".overlay-top", 1.6, {
+        height: 0,
+        ease: "expo.inOut",
+        stagger: 0.4,
+      })
+      .to(".overlay-bottom", 1.6, {
+        width: 0,
+        ease: "expo.inOut",
+        delay: -0.8,
+        stagger: {
+          amount: 0.4,
+        },
+      })
+      .to(".intro-overlay", 1, { css: { display: "none" } })
+      .from(".case-image img", 1.6, {
+        scale: 1.4,
+        ease: "expo.inOut",
+        delay: -2,
+        stagger: {
+          amount: 0.4,
+        },
+      });
   });
   return (
     <div className="App ">
+      <Intro />
       <Header />
       <Banner />
       <Cases />
